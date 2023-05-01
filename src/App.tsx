88d02a4396e00a59cbb52765d2916baee18e98c1
Marco Milza <marco@192.168.1.52>
2023-05-01 23:15:36 +0200

    removed unused stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import "./global.scss";
-import { useState } from "react";
-import { Item } from "./models/mainModels";
-import { initialShopList } from "./utils/mockData";
 import LogInComp from "./components/LogInComp";
-import Navbar from "./components/Navbar";
 import Router from "./Router";
 
 export type AddItem = (item: { name: string; quantity: string }) => void;
 
 function App() {
-  const [shoppingList, setShoppingList] = useState<Item[]>(initialShopList);
-
   return (
     <div className="App">
       <RouterProvider router={Router} />
